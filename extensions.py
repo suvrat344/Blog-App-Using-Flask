eae819c4f806302ebc721cd01c3a3b93b2aaf646
Suvrat Chauhan <chauhansuvrat344@gmail.com>
2025-02-03 00:23:43 +0530

    Add extension file

--- extensions.py
+++ extensions.py
@@ -1,7 +1,9 @@
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 
 bcrypt = Bcrypt()
 db = SQLAlchemy()
 login_manager = LoginManager()
+mail = Mail()
